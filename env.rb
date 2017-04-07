1af55c1787205669c524add0109d1ff93b6f9d3e
Pawel Godziuk <pawel.godziuk@gmail.com>
2017-04-07 12:42:49 +0200

    stworzenie pliku do łączenia się z bazą danych czat_db

---
+++ env.rb
@@ -0,0 +1,10 @@
+require 'active_record'
+
+Dir.glob('./models/*.rb').each do |file|
+  require file
+end
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'postgresql',
+  database: 'czat_db'
+  )
